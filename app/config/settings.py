32b8b699fddd94f12c22219bb70576bc78d7f255
Gentian Demaj <gentiani101@gmail.com>
2021-10-17 00:48:25 +0200

    feat: add settings.json

--- app/config/settings.py
+++ app/config/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env.str('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool('DEBUG', default=False)
+DEBUG = env.bool('DJANGO_DEBUG', default=False)
 
 ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', 'localhost']
 
